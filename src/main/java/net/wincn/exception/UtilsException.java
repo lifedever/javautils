2349fb4d040cce3c9f2b3c818a08a8c932caefdb
葛方帅 <gefangshuai@163.com>
2013-12-10 13:31:18 +0800

    重构工具类

---
+++ src/main/java/net/wincn/exception/UtilsException.java
@@ -0,0 +1,13 @@
+package net.wincn.exception;
+
+public class UtilsException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public UtilsException() {
+
+	}
+
+	public UtilsException(String msg) {
+		super(msg);
+	}
+}
