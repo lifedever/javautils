4fca9618b34783a6605aeaa577d7d4d743b13a71
葛方帅 <gefangshuai@163.com>
2013-12-28 11:50:21 +0800

    加入过滤无效字符方法： String stripNonValidXMLCharacters(String in)

--- src/main/java/net/wincn/utils/string/StrUtils.java
+++ src/main/java/net/wincn/utils/string/StrUtils.java
@@ -284,5 +284,27 @@ public class StrUtils {
 		}
 		return intArr;
 	}
+	
+	  /**
+	   * 过滤无效字符
+	   * @param in
+	   * @return
+	   */
+	  public String stripNonValidXMLCharacters(String in) {
+	      StringBuffer out = new StringBuffer(); // Used to hold the output.
+	      char current; // Used to reference the current character.
 
+	      if (in == null || ("".equals(in)))
+	          return ""; // vacancy test.
+	      for (int i = 0; i < in.length(); i++) {
+	          current = in.charAt(i); // NOTE: No IndexOutOfBoundsException caught
+	                                  // here; it should not happen.
+	          if ((current == 0x9) || (current == 0xA) || (current == 0xD)
+	                  || ((current >= 0x20) && (current <= 0xD7FF))
+	                  || ((current >= 0xE000) && (current <= 0xFFFD))
+	                  || ((current >= 0x10000) && (current <= 0x10FFFF)))
+	              out.append(current);
+	      }
+	      return out.toString();
+	  }
 }
